2f60f2c4ae8f240e4224601fbd62a86caed19daf
suvorovjr <pglavan1998@mail.ru>
2024-04-29 20:22:49 +0300

    add celery tasks.py

---
+++ merge_files/tasks.py
@@ -0,0 +1,19 @@
+from celery import shared_task
+from .services import MergeFiles
+from merge_files.models import UploadFiles, MergedFile
+from django.core.files import File
+import os
+
+
+@shared_task
+def merge_files(files_pk):
+    obj = UploadFiles.objects.get(pk=files_pk)
+    san_file = obj.file1.path
+    market_file = obj.file2.path
+    merge_file_client = MergeFiles(san_file=san_file, market_file=market_file)
+    path_to_merge_file = merge_file_client.merge_files()
+    merge_file_instance = MergedFile()
+    with open(path_to_merge_file, 'rb') as file:
+        django_file = File(file)
+        merge_file_instance.merge_file.save(os.path.basename(path_to_merge_file), django_file, save=True)
+        merge_file_instance.save()
