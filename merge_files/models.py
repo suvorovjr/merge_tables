306a2767bc66fd294583d3f7d0af5017c3f349b5
suvorovjr <pglavan1998@mail.ru>
2024-04-30 13:37:06 +0300

    add change name of files_field

--- merge_files/models.py
+++ merge_files/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class UploadFiles(models.Model):
     upload_date = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Дата и время загрузки')
-    file1 = models.FileField(upload_to='exel_files/', verbose_name='Файл Sanbest')
-    file2 = models.FileField(upload_to='exel_files/', verbose_name='Файл Я.Маркета')
+    san_file = models.FileField(upload_to='exel_files/', verbose_name='Файл Sanbest')
+    market_file = models.FileField(upload_to='exel_files/', verbose_name='Файл Я.Маркета')
 
     def __str__(self):
         return f'Файл Sanbest - {self.file1}. Файл Я.Маркета {self.file2}'
