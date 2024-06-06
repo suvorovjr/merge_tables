84bf26cd7adef7829e823ae82cd444846f95ec46
suvorovjr <pglavan1998@mail.ru>
2024-06-06 17:17:57 +0300

    add changes in urls report app

--- report/urls.py
+++ report/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .apps import ReportConfig
+from .views import ReportCreateView
 
 app_name = ReportConfig.name
 
 urlpatterns = [
+    path('', ReportCreateView.as_view(), name='create')
 ]
