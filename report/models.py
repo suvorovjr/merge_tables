23d0e658c87b61f8f87c3bdff3dde25decf4317b
suvorovjr <pglavan1998@mail.ru>
2024-06-06 16:55:07 +0300

    add ReportFile model

--- report/models.py
+++ report/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ReportFile(models.Model):
+    upload_date = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Дата и время загрузки')
+    file = models.FileField(upload_to='files', verbose_name='Файл')
+
+    class Meta:
+        verbose_name = 'Загруженный отчет'
+        verbose_name_plural = 'Загруженные отчеты'
