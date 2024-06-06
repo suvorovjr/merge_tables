1b57cf26a3b0cd2dfec0c3f281242495d6eaf4db
suvorovjr <pglavan1998@mail.ru>
2024-06-06 17:14:25 +0300

    add ReportCreateView

--- report/views.py
+++ report/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import ReportForm
+from django.urls import reverse_lazy
+from .models import ReportFile
 
-# Create your views here.
+
+class ReportCreateView(CreateView):
+    form_class = ReportForm
+    template_name = 'report/report_form.html'
+    success_url = reverse_lazy('merge_files:files_list')
