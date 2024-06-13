97a7225601fc3088a891229fe812c487a776feab
suvorovjr <pglavan1998@mail.ru>
2024-06-13 17:51:46 +0300

    add ProductSubmit url

--- report/urls.py
+++ report/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .apps import ReportConfig
-from .views import ReportCreateView, ReportListView, ReportDetailView, load_brands
+from .views import ReportCreateView, ReportListView, ReportDetailView, load_brands, ProductSubmitView
 
 app_name = ReportConfig.name
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', ReportCreateView.as_view(), name='create'),
     path('list/', ReportListView.as_view(), name='list'),
     path('detail/<int:pk>/', ReportDetailView.as_view(), name='detail'),
-    path('api/brands/', load_brands, name='load_brands')
+    path('api/brands/', load_brands, name='load_brands'),
+    path('api/get-data/', ProductSubmitView.as_view(), name='get_data')
 ]
